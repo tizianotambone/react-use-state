27c92a3279c8880fca365af10e152a22155f8452
tizianotambone <tambonetiziano@gmail.com>
2025-05-08 19:59:26 +0200

    [UPD] fix appjsx

---
+++ react-use-state/src/assets/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ListLanguages from './ListLanguages'
+
+const Main = () => {
+    return (
+        <div className="container">
+            <h1>Learn Web development</h1>
+
+            <div className="tabs">
+                <ListLanguages />
+
+            </div>
+            
+        </div>
+    )
+}
+
+
+
+
+export default Main
